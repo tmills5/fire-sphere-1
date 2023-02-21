d149cac64e50d06094acfa23a72d26f2e0778b72
Merle Tyson Mills <tysonmills5@gmail.com>
2023-02-21 15:19:00 -0600

    working on nested objs in profile

--- app/serializers/station_serializer.rb
+++ app/serializers/station_serializer.rb
@@ -1,3 +1,6 @@
 class StationSerializer < ActiveModel::Serializer
   attributes :id, :station_number, :address
+
+  has_many :vehicles
+  has_many :users, through: :vehicles
 end
--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 const UserProfile = ({ user }) => {
-  const { first_name, last_name, username, rank, shift, vehicle_id, is_admin, home_address } = user;
+  const { first_name, last_name, username, rank, shift, is_admin, home_address} = user;
+  // const { year } = user.vehicle;
   console.log("UserProfile user: ", user)
-
+  console.log("vehicle: ", user.vehicle.model)
 
   return (
     <>
       <h1>{username}</h1>
       <p>{last_name}, {first_name}</p>
       <p>{rank} - {shift} shift</p>
-      <p>Vehicle: {vehicle_id}</p>
+      {/* <p>Vehicle: {user.vehicle}</p> */}
       <p>Home Address: {home_address}</p>
     </>
   )
