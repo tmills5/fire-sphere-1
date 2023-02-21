558783d532385bdf5546b7d0b997862bb7fc529b
Merle Tyson Mills <tysonmills5@gmail.com>
2023-02-21 14:47:40 -0600

    user info in profile

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const UserProfile = ({ user }) => {
-  const { username, email, id } = user;
+  const { first_name, last_name, username, rank, shift, vehicle_id, is_admin, home_address } = user;
+  console.log("UserProfile user: ", user)
+
 
   return (
-    <h1>User Profile `${user.username}`</h1>
+    <>
+      <h1>{username}</h1>
+      <p>{last_name}, {first_name}</p>
+      <p>{rank} - {shift} shift</p>
+      <p>Vehicle: {vehicle_id}</p>
+      <p>Home Address: {home_address}</p>
+    </>
   )
 };
 
